31e30f01479a21aaad2d498a67b2a3ad441ab802
jonathasmedina <emaildojonathas@gmail.com>
2018-05-24 10:22:24 -0400

    -

--- app/src/main/java/com/example/jonathas/firebasecrud/MainActivity.java
+++ app/src/main/java/com/example/jonathas/firebasecrud/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.jonathas.firebasecrud;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +30,13 @@ public class MainActivity extends AppCompatActivity {
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
 
+
+    private List<Pessoa> listPessoa = new ArrayList<Pessoa>();
+    private ArrayAdapter<Pessoa> pessoaArrayAdapter;
+
+    Pessoa pessoaSelecionada;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,12 +46,51 @@ public class MainActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.listView1);
 
         inicializarFirebase();
+
+        eventoDatabase();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                pessoaSelecionada = (Pessoa) parent.getItemAtPosition(position);
+                edNome.setText(pessoaSelecionada.getNome());
+                edEmail.setText(pessoaSelecionada.getEmail());
+
+            }
+        });
+    }
+
+    private void eventoDatabase() {
+        databaseReference.child("Pessoa").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                    listPessoa.clear();
+
+                    for(DataSnapshot objSnapshot: dataSnapshot.getChildren()){
+                        Pessoa p = objSnapshot.getValue(Pessoa.class);
+                        listPessoa.add(p);
+                    }
+
+                    pessoaArrayAdapter = new ArrayAdapter<Pessoa>(MainActivity.this,
+                            android.R.layout.simple_list_item_1, listPessoa);
+                    listView.setAdapter(pessoaArrayAdapter);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void inicializarFirebase() {
         FirebaseApp.initializeApp(MainActivity.this);
         firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseDatabase.setPersistenceEnabled(true);
         databaseReference = firebaseDatabase.getReference();
+
+
     }
 
     @Override
@@ -57,6 +112,28 @@ public class MainActivity extends AppCompatActivity {
             databaseReference.child("Pessoa").child(p.getId()).setValue(p);
             limparCampos();
         }
+        if (id == R.id.menu_atualiza) {
+            Pessoa p = new Pessoa();
+            p.setId(pessoaSelecionada.getId());
+            p.setNome(edNome.getText().toString());
+            p.setEmail(edEmail.getText().toString());
+
+            databaseReference.child("Pessoa").child(p.getId()).setValue(p);
+
+            limparCampos();
+        }
+        if (id == R.id.menu_deleta) {
+            Pessoa p = new Pessoa();
+            p.setId(pessoaSelecionada.getId());
+
+            databaseReference.child("Pessoa").child(p.getId()).removeValue();
+        }
+        if (id == R.id.menu_pesquisa) {
+            Intent intent = new Intent(MainActivity.this, Pesquisa.class);
+            startActivity(intent);
+        }
+
+
         return true;
     }
 
@@ -64,4 +141,7 @@ public class MainActivity extends AppCompatActivity {
         edNome.setText("");
         edEmail.setText("");
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/jonathas/firebasecrud/Pesquisa.java
@@ -0,0 +1,102 @@
+package com.example.jonathas.firebasecrud;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pesquisa extends AppCompatActivity {
+
+    EditText edPesquisa;
+    ListView listView;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    List<Pessoa> pessoaList = new ArrayList<>();
+    ArrayAdapter<Pessoa> arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pesquisa);
+
+        edPesquisa = findViewById(R.id.edPesquisa);
+        listView = findViewById(R.id.listViewPesquisa);
+
+        inicializarFirebase();
+        eventoEdit();
+    }
+
+    private void eventoEdit() {
+        edPesquisa.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String palavra = edPesquisa.getText().toString().trim();
+                pesquisarPalavra(palavra);
+            }
+        });
+    }
+
+    private void pesquisarPalavra(String palavra) {
+        Query query;
+        if (palavra.equals("")) {
+            query = databaseReference.child("Pessoa").orderByChild("nome");
+        } else {
+            query = databaseReference.child("Pessoa").startAt(palavra).endAt(palavra+"\uf8ff");
+        }
+
+        pessoaList.clear();
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot objSnapshot: dataSnapshot.getChildren()) {
+                    Pessoa p = objSnapshot.getValue(Pessoa.class);
+                    pessoaList.add(p);
+                }
+
+                arrayAdapter = new ArrayAdapter<Pessoa>(Pesquisa.this,
+                        android.R.layout.simple_list_item_1, pessoaList);
+
+                listView.setAdapter(arrayAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+
+    }
+
+    private void inicializarFirebase() {
+        FirebaseApp.initializeApp(Pesquisa.this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+    }
+}
